29ad02fdd1f785ae74e14b4b755f01280cb1c224
zhuxiaoyun <zhuxiaoyun@mogoroom.com>
2019-05-27 23:27:35 +0800

    dialog

---
+++ src/components/dialog/toast.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="toast">
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'GoodCard',
+  props: {
+  },
+  data () {
+    return {
+    }
+  },
+  methods: {
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+</style>
